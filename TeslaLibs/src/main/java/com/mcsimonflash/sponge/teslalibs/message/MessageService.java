42a57439f2c29c85563ea67bc77305e06f35e1bb
runescapejon <jonsimpletv@gmail.com>
2020-12-25 14:12:26 -0500

    Fixed Resourcebundle from thinking that files are under ISO-8859 displaying weird text and symbol in-game. I found a source to help address the problem, credit is in the class on where I found the source.

--- TeslaLibs/src/main/java/com/mcsimonflash/sponge/teslalibs/message/MessageService.java
+++ TeslaLibs/src/main/java/com/mcsimonflash/sponge/teslalibs/message/MessageService.java
@@ -2,18 +2,26 @@ package com.mcsimonflash.sponge.teslalibs.message;
 
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.plugin.PluginContainer;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 public class MessageService {
 
     private final ClassLoader loader;
     private final String name;
+ 
 
     /**
      * @see MessageService#of(ClassLoader, String)
@@ -56,20 +64,30 @@ public class MessageService {
     /**
      * Returns the {@link ResourceBundle} for the given {@link Locale} or throws
      * a {@link java.util.MissingResourceException} if no bundle could be found.
+     * @throws IOException 
+     * @throws InstantiationException 
+     * @throws IllegalAccessException 
      *
      * @see ResourceBundle#getBundle(String, Locale, ClassLoader)
      */
-    public ResourceBundle getBundle(Locale locale) {
-        return ResourceBundle.getBundle(name, locale, loader);
+    
+    //Using UTF8Control to workaround the issue with ResourceBundle from encoding the file under ISO-8859
+
+    public ResourceBundle getBundle(Locale locale)  {
+    	  return ResourceBundle.getBundle(name, locale, loader, new UTF8Control());
     }
 
     /**
      * Creates a new {@link Message} from the string retrieved from the locale's
      * {@link ResourceBundle} and this service's prefix and suffix. If the
      * bundle does not contain the given key, the key itself is used.
+     * @throws IOException 
+     * @throws InstantiationException 
+     * @throws IllegalAccessException 
      *
      * @see MessageService#getBundle(Locale)
      */
+    
     public Message get(String key, Locale locale) {
         ResourceBundle bundle = getBundle(locale);
         return Message.of(bundle.containsKey(key) ? bundle.getString(key) : key);
